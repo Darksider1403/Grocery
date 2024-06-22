ebcb5c20c79599045fb3335327f3c6b0e6326d7a
quocthai2612003 <21130502@st.hcmuaf.edu.vn>
2024-06-22 11:39:04 +0700

    comment code fcm

--- app/src/main/java/com/example/grocery/Constants.java
+++ app/src/main/java/com/example/grocery/Constants.java
@@ -2,6 +2,7 @@ package com.example.grocery;
 
 public class Constants {
     // product categories
+    //TODO quoc key clip 18 min 14
     public static final String FCM_KEY ="AIzaSyA_FnE0etNau1DUIsfD93aLK2zenenX0Hg";
     public static final String FCM_TOPIC ="PUSH_NOTIFICATIONS";
 
