4e868c3b05f7c1ee8132829dfc3fcc1178072de0
leemanosi <namblue789@gmail.com>
2024-06-24 00:20:14 +0700

    22.5

--- app/src/main/java/com/example/grocery/adapters/AdapterPromotionShop.java
+++ app/src/main/java/com/example/grocery/adapters/AdapterPromotionShop.java
@@ -1,17 +1,28 @@
 package com.example.grocery.adapters;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.grocery.R;
+import com.example.grocery.activities.AddPromotionCodeActivity;
 import com.example.grocery.models.ModelPromotion;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -20,9 +31,18 @@ public class AdapterPromotionShop extends RecyclerView.Adapter<AdapterPromotionS
         private Context context;
         private ArrayList<ModelPromotion> promotionArrayList;
 
+        private ProgressDialog progressDialog;
+        private FirebaseAuth firebaseAuth;
+
     public AdapterPromotionShop(Context context, ArrayList<ModelPromotion> promotionArrayList) {
         this.context = context;
         this.promotionArrayList = promotionArrayList;
+
+        this.firebaseAuth = FirebaseAuth.getInstance();
+
+        this.progressDialog = new ProgressDialog(context);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setCanceledOnTouchOutside(false);
     }
 
     @NonNull
@@ -52,6 +72,69 @@ public class AdapterPromotionShop extends RecyclerView.Adapter<AdapterPromotionS
         holder.minimumOrderPriceTv.setText(minimumOrderPrice);
         holder.promoCodeTv.setText("Code: "+promoCode);
         holder.expireDateTv.setText("Expire Date: "+expireDate);
+
+        //handle click, show Edit/Delete dialog
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editDeleteDialog(modelPromotion, holder);
+            }
+        });
+    }
+
+    private void editDeleteDialog(ModelPromotion modelPromotion, HolderPromotionShop holder) {
+        //options to display in dialog
+        String[] options = {"Edit", "Delete"};
+        //dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Choose option")
+                .setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        //handle clicks
+                        if (i==0){
+                            //Edit clicked
+                            editPromoCode(modelPromotion);
+                        }
+                        else if (i==1) {
+                            //Delete clicked
+                        }
+                    }
+                })
+                .show();
+    }
+
+    private void deletePromoCode(ModelPromotion modelPromotion) {
+        //show progress bar
+        progressDialog.setMessage("Deleting Promotion Code...");
+        progressDialog.show();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("User");
+        ref.child(firebaseAuth.getUid()).child("Promotions").child(modelPromotion.getId())
+                .removeValue()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //deleted
+                        progressDialog.dismiss();
+                        Toast.makeText(context, "Deleting...", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //failed deleting
+                        progressDialog.dismiss();
+                        Toast.makeText(context, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void editPromoCode(ModelPromotion modelPromotion) {
+        //start and pass data to AddPromoCodeActivity to edit
+        Intent intent = new Intent(context, AddPromotionCodeActivity.class);
+        intent.putExtra("promoId", modelPromotion.getId()); //will use id to update promo code
+        context.startActivity(intent);
     }
 
     @Override
